7ce54db0187f99fa7c49536d1bca845d0bfc964f
Ryan Raghani <raghaniryan@gmail.com>
2025-11-23 04:23:06 -0800

    second part of package

---
+++ elevator_types_body.vhd
@@ -0,0 +1,9 @@
+-- Ryan Raghani – 301623888; Danny Woo – 301613129; Mitchell Kieper – 301590274;
+
+
+-- second part of the package file to declare direction_t
+library ieee;
+use ieee.std_logic_1164.all;
+
+package body elevator_types is
+end package body elevator_types;
