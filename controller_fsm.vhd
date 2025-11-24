21b50981ee4ec212ba0672990849552c8eb91977
Ryan Raghani <raghaniryan@gmail.com>
2025-11-23 19:13:22 -0800

    big push while i debug

--- controller_fsm.vhd
+++ controller_fsm.vhd
@@ -1,4 +1,4 @@
--- Ryan Raghani – 301623888; Danny Woo – 301613129; Mitchell Kieper – 301590274;
+-- Ryan Raghani – 301623888; Danny Woo – 301613129; Mitchell Kieper – 301590274
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -27,10 +27,9 @@ entity controller_fsm is
     );
 end controller_fsm;
 
-
 architecture rtl of controller_fsm is
 
-    -- State Encodings
+    -- States
     type state_t is (
         INIT,
         IDLE,
@@ -45,112 +44,103 @@ architecture rtl of controller_fsm is
 
     signal state, next_state : state_t;
 
-    -- Internal Registers
+    -- Registers
     signal floor_reg    : integer range 0 to 7 := 0;
     signal dir_reg      : direction_t := DIR_IDLE;
 
     signal travel_timer : integer range 0 to 2 := 0;
     signal door_timer   : integer range 0 to 3 := 0;
 
-    -- ESTOP TOGGLE LOGIC
-    signal estop_prev  : std_logic := '1';
-    signal estop_edge  : std_logic := '0';
+    -- estop toggle
+    signal estop_prev   : std_logic := '1';
+    signal estop_edge   : std_logic := '0';
     signal estop_active : std_logic := '0';
 
+    -- Output assignments
 begin
-
     current_floor <= floor_reg;
     direction     <= dir_reg;
 
-
-    -- ESTOP Edge Detect
+    -- ESTOP edge detect
     process(clk)
     begin
         if rising_edge(clk) then
             estop_edge <= '0';
-
             if estop_prev = '1' and estop_btn = '0' then
-                estop_edge <= '1';   -- falling edge toggle
+                estop_edge <= '1';
             end if;
-
             estop_prev <= estop_btn;
         end if;
     end process;
 
 
+    -- Combinational block for next_state and combinational outputs
+	 process(state, pending, sched_dir, sched_floor, door_timer, floor_reg)
+	 begin
+		-- default values
+		next_state    <= state;
+		door_open_led <= '0';
 
-    -- Next state logic which is all combinational 
-    process(state, pending, sched_floor, sched_dir, door_timer, floor_reg)
-    begin
-        next_state <= state;   -- default
+		case state is
 
-        case state is
+			-- INIT 
+			when INIT =>
+				next_state <= IDLE;
 
-            when INIT =>
-                next_state <= IDLE;
+			-- IDLE 
+			when IDLE =>
+            if pending /= "00000000" then
+                next_state <= SCHEDULE;
+            end if;
 
-            when IDLE =>
-                if pending /= "00000000" then
-                    next_state <= SCHEDULE;
-                else
-                    next_state <= IDLE;
-                end if;
+			-- Schedule
+			when SCHEDULE =>
+            case sched_dir is
+                when DIR_UP   => next_state <= MOVE_UP;
+                when DIR_DOWN => next_state <= MOVE_DOWN;
+                when others   => next_state <= IDLE;
+            end case;
 
-            when SCHEDULE =>
-                case sched_dir is
-                    when DIR_UP   => next_state <= MOVE_UP;
-                    when DIR_DOWN => next_state <= MOVE_DOWN;
-                    when others   => next_state <= IDLE;
-                end case;
+			-- Move up
+			when MOVE_UP =>
+            if floor_reg = sched_floor then
+                next_state <= ARRIVE;
+            end if;
 
-            when MOVE_UP =>
-                if floor_reg = sched_floor then
-                    next_state <= ARRIVE;
-                else
-                    next_state <= MOVE_UP;
-                end if;
+			-- Move down
+			when MOVE_DOWN =>
+            if floor_reg = sched_floor then
+                next_state <= ARRIVE;
+            end if;
 
-            when MOVE_DOWN =>
-                if floor_reg = sched_floor then
-                    next_state <= ARRIVE;
-                else
-                    next_state <= MOVE_DOWN;
-                end if;
+			-- Arrive
+			when ARRIVE =>
+            clear_floor(floor_reg) <= '1';   -- one cycle
+            next_state <= DOOR_OPEN;
 
-            when ARRIVE =>
-                next_state <= DOOR_OPEN;
+			-- Door open
+			when DOOR_OPEN =>
+            door_open_led <= '1';
+            if door_timer = 3 then
+                next_state <= DOOR_CLOSE;
+            end if;
 
-            when DOOR_OPEN =>
-                if door_timer = 3 then
-                    next_state <= DOOR_CLOSE;
-                else
-                    next_state <= DOOR_OPEN;
-                end if;
+			-- Door close
+        when DOOR_CLOSE =>
+            next_state <= IDLE;
 
-            when DOOR_CLOSE =>
-                next_state <= IDLE;
+			-- ESTOP 
+        when ESTOP =>
+            next_state <= ESTOP;
 
-            when ESTOP =>
-                next_state <= ESTOP;
-
-        end case;
-    end process;
+		end case;
+	end process;
 
 
-
-    -- Sequencial process
+    -- Sequenctial block where all registers update
     process(clk)
     begin
         if rising_edge(clk) then
-
-            clear_floor   <= (others => '0');
-            door_open_led <= '0';
-
-            -- toggle ESTOP
-            if estop_edge = '1' then
-                estop_active <= not estop_active;
-            end if;
-
 
             -- Hard reset
             if reset_hard = '1' then
@@ -161,21 +151,29 @@ begin
                 door_timer   <= 0;
                 estop_active <= '0';
 
+            -- ESTOP toggle latching
+            elsif estop_edge = '1' then
+                estop_active <= not estop_active;
 
-            -- ESTOP 
+
             elsif estop_active = '1' then
                 state        <= ESTOP;
                 travel_timer <= 0;
                 door_timer   <= 0;
 
+            -- Soft reset
+            elsif reset_soft = '1' then
+                state        <= IDLE;
+                travel_timer <= 0;
+                door_timer   <= 0;
 
             -- Normal operation
             else
 
-                -- Update state first
+                -- update state
                 state <= next_state;
 
-                -- Travel timer 
+                -- Movement (uses next_state)
                 if tick_1hz = '1' then
 
                     case next_state is
@@ -197,8 +195,7 @@ begin
                             travel_timer <= 0;
                     end case;
 
-
-                    -- Door timer
+                    -- DOOR TIMER
                     case next_state is
                         when DOOR_OPEN =>
                             door_timer <= door_timer + 1;
@@ -208,22 +205,9 @@ begin
                             null;
                     end case;
 
-                end if;
+                end if; -- tick_1hz
 
-
-                -- Clear floor when arrives
-                if next_state = ARRIVE then
-                    clear_floor(floor_reg) <= '1';
-                end if;
-
-
-                -- Door LED
-                if next_state = DOOR_OPEN then
-                    door_open_led <= '1';
-                end if;
-
-
-                -- Direction logic
+                -- Direction register
                 case next_state is
                     when SCHEDULE =>
                         dir_reg <= sched_dir;
