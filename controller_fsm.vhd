b2060f17b01683e9fa4754b85f713c0b0b5a6f89
Ryan Raghani <raghaniryan@gmail.com>
2025-11-23 16:55:31 -0800

    rewrote a lot of code to fix the immediate clearing of floors. there are no more illegal transitions now so the latching should work with a req_latch quick rewrite changing simple active high logic. now there is no more bouncing in between states... hopefully

--- controller_fsm.vhd
+++ controller_fsm.vhd
@@ -6,39 +6,31 @@ use ieee.numeric_std.all;
 use work.elevator_types.all;
 
 entity controller_fsm is
-    port (
-        clk         : in  std_logic;           -- 50 MHz
-        tick_1hz    : in  std_logic;           -- 1-second pulse
+    port(
+        clk          : in  std_logic;
+        tick_1hz     : in  std_logic;
 
-        -- Reset signals
-        reset_hard  : in  std_logic;           -- KEY3 active-high
-        reset_soft  : in  std_logic;           -- KEY2 active-high
+        reset_hard   : in  std_logic;
+        reset_soft   : in  std_logic;
 
-        -- ESTOP toggle button (KEY1)
-        estop_btn   : in  std_logic;           -- active-low physical input
+        estop_btn    : in  std_logic;
 
-        -- Data from req_latch
-        pending      : in  std_logic_vector(7 downto 0);
+        pending       : in  std_logic_vector(7 downto 0);
+        clear_floor   : out std_logic_vector(7 downto 0);
 
-        -- Outputs TO req_latch
-        clear_floor  : out std_logic_vector(7 downto 0);
+        sched_dir     : in direction_t;
+        sched_floor   : in integer range 0 to 7;
 
-        -- Inputs from scheduler
-        sched_dir    : in  direction_t;
-        sched_floor  : in  integer range 0 to 7;
-
-        -- Internal tracking
         current_floor : out integer range 0 to 7;
         direction     : out direction_t;
-
-        -- Door indicator
         door_open_led : out std_logic
     );
 end controller_fsm;
 
+
 architecture rtl of controller_fsm is
 
-    -- FSM States
+    -- State Encodings
     type state_t is (
         INIT,
         IDLE,
@@ -54,44 +46,43 @@ architecture rtl of controller_fsm is
     signal state, next_state : state_t;
 
     -- Internal Registers
-    signal floor_reg      : integer range 0 to 7 := 0;
-    signal dir_reg        : direction_t := DIR_IDLE;
+    signal floor_reg    : integer range 0 to 7 := 0;
+    signal dir_reg      : direction_t := DIR_IDLE;
 
-    signal travel_timer   : integer range 0 to 2 := 0;   -- 2-second travel
-    signal door_timer     : integer range 0 to 3 := 0;   -- 3-second door open
+    signal travel_timer : integer range 0 to 2 := 0;
+    signal door_timer   : integer range 0 to 3 := 0;
 
-    -- ESTOP toggle logic
-    signal estop_prev     : std_logic := '1';  -- track KEY1 previous
-    signal estop_edge     : std_logic := '0';
-    signal estop_active   : std_logic := '0';  -- latched toggle flip-flop
+    -- ESTOP TOGGLE LOGIC
+    signal estop_prev  : std_logic := '1';
+    signal estop_edge  : std_logic := '0';
+    signal estop_active : std_logic := '0';
 
 begin
 
-    -- Output wiring
     current_floor <= floor_reg;
     direction     <= dir_reg;
 
-    -- ESTOP Toggle Detection
+
+    -- ESTOP Edge Detect
     process(clk)
-begin
-    if rising_edge(clk) then
-
-        estop_edge <= '0';
-
-        if estop_prev = '1' and estop_btn = '0' then
-            estop_edge <= '1';   -- falling edge detected
-        end if;
-
-        estop_prev <= estop_btn;
-
-    end if;
-end process;
-
-
-    -- Next state combinational logic 
-    process(state, pending, sched_dir, sched_floor, door_timer, floor_reg)
     begin
-        next_state <= state;
+        if rising_edge(clk) then
+            estop_edge <= '0';
+
+            if estop_prev = '1' and estop_btn = '0' then
+                estop_edge <= '1';   -- falling edge toggle
+            end if;
+
+            estop_prev <= estop_btn;
+        end if;
+    end process;
+
+
+
+    -- Next state logic which is all combinational 
+    process(state, pending, sched_floor, sched_dir, door_timer, floor_reg)
+    begin
+        next_state <= state;   -- default
 
         case state is
 
@@ -101,6 +92,8 @@ end process;
             when IDLE =>
                 if pending /= "00000000" then
                     next_state <= SCHEDULE;
+                else
+                    next_state <= IDLE;
                 end if;
 
             when SCHEDULE =>
@@ -111,16 +104,28 @@ end process;
                 end case;
 
             when MOVE_UP =>
-                if floor_reg = sched_floor then next_state <= ARRIVE; end if;
+                if floor_reg = sched_floor then
+                    next_state <= ARRIVE;
+                else
+                    next_state <= MOVE_UP;
+                end if;
 
             when MOVE_DOWN =>
-                if floor_reg = sched_floor then next_state <= ARRIVE; end if;
+                if floor_reg = sched_floor then
+                    next_state <= ARRIVE;
+                else
+                    next_state <= MOVE_DOWN;
+                end if;
 
             when ARRIVE =>
                 next_state <= DOOR_OPEN;
 
             when DOOR_OPEN =>
-                if door_timer = 3 then next_state <= DOOR_CLOSE; end if;
+                if door_timer = 3 then
+                    next_state <= DOOR_CLOSE;
+                else
+                    next_state <= DOOR_OPEN;
+                end if;
 
             when DOOR_CLOSE =>
                 next_state <= IDLE;
@@ -132,42 +137,48 @@ end process;
     end process;
 
 
-    -- Single sequential process 
+
+    -- Sequencial process
     process(clk)
     begin
         if rising_edge(clk) then
 
-            -- Default outputs
+            -- Default outputs for each cycle
             clear_floor   <= (others => '0');
             door_open_led <= '0';
-				
-				if estop_edge = '1' then
-					estop_active <= not estop_active;
-				end if;
 
-            -- Hard reset
+            -- ESTOP toggle
+            if estop_edge = '1' then
+                estop_active <= not estop_active;
+            end if;
+
+
+            -- Hard reset takes precidence
             if reset_hard = '1' then
+
                 state        <= INIT;
-                dir_reg      <= DIR_IDLE;
                 floor_reg    <= 0;
+                dir_reg      <= DIR_IDLE;
                 travel_timer <= 0;
                 door_timer   <= 0;
                 estop_active <= '0';
 
-            -- ESTOP
+            -- ESTOP locked state
             elsif estop_active = '1' then
-                state <= ESTOP;
+
+                state        <= ESTOP;
                 travel_timer <= 0;
                 door_timer   <= 0;
 
-            -- Normal fsm logic
+            -- Normal FSM Progression
             else
                 state <= next_state;
 
-                -- Travel logic
+                -- Travel timing (2 seconds per floor)
                 if tick_1hz = '1' then
 
                     case state is
+
                         when MOVE_UP =>
                             travel_timer <= travel_timer + 1;
                             if travel_timer = 2 then
@@ -184,42 +195,52 @@ end process;
 
                         when others =>
                             travel_timer <= 0;
+
                     end case;
 
-                    -- Door timing
+                    -- Door timer (3 seconds)
                     case state is
                         when DOOR_OPEN =>
                             door_timer <= door_timer + 1;
-                        when DOOR_CLOSE | IDLE =>
+                        when IDLE | DOOR_CLOSE =>
                             door_timer <= 0;
                         when others =>
                             null;
                     end case;
+
                 end if;
 
-                -- Clear request
+
+                -- Clear the floor request upon arrival
                 if state = ARRIVE then
                     clear_floor(floor_reg) <= '1';
                 end if;
 
+					 
                 -- Door LED
                 if state = DOOR_OPEN then
                     door_open_led <= '1';
                 end if;
 
-                -- Direction output
+
+                -- Direction register updates
                 case state is
                     when SCHEDULE =>
                         dir_reg <= sched_dir;
+
                     when MOVE_UP =>
                         dir_reg <= DIR_UP;
+
                     when MOVE_DOWN =>
                         dir_reg <= DIR_DOWN;
+
                     when others =>
                         dir_reg <= DIR_IDLE;
                 end case;
-            end if;
-        end if;
+
+            end if; -- normal operation
+
+        end if; -- rising edge
     end process;
 
 end rtl;
