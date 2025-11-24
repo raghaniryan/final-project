25bd7e3ed16731846ab6f3021b24e59010eea70a
Ryan Raghani <raghaniryan@gmail.com>
2025-11-23 22:09:32 -0800

    this file is no longer used in the project

--- elevator_types_body.vhd
+++ elevator_types_body.vhd
@@ -1,9 +1,4 @@
 -- Ryan Raghani – 301623888; Danny Woo – 301613129; Mitchell Kieper – 301590274;
 
 
--- second part of the package file to declare direction_t
-library ieee;
-use ieee.std_logic_1164.all;
-
-package body elevator_types is
-end package body elevator_types;
+-- no longer used in the project
