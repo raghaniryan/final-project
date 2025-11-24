4df9f740cd96aa02674554754977c0a047957e51
Ryan Raghani <raghaniryan@gmail.com>
2025-11-23 17:03:25 -0800

    i had to change some of the code as it was outputting 20ns pulse instead of 1s toggles.

--- clk_div.vhd
+++ clk_div.vhd
@@ -6,39 +6,43 @@ use ieee.numeric_std.all;
 use work.elevator_types.all;
 
 
+-- tick_1hz is now a 1 Hz toggle
 entity clk_div is
     generic(
-        DIVISOR : integer := 50_000_000   -- 50 MHz turned into 1 Hz
+        DIVISOR : integer := 50_000_000
     );
     port(
-        clk_in   : in  std_logic;         -- 50 MHz system clock
-        reset_n  : in  std_logic;         -- active-low reset
-        tick_1hz : out std_logic          -- 1 cycle pulse every second
+        clk_in   : in  std_logic;
+        reset_n  : in  std_logic;
+        tick_1hz : out std_logic
     );
 end clk_div;
 
 architecture rtl of clk_div is
-    signal count : unsigned(31 downto 0) := (others => '0');
+    signal count     : unsigned(31 downto 0) := (others => '0');
+    signal tick_reg  : std_logic := '0';      -- stores current 1 Hz state
 begin
 
     process(clk_in, reset_n)
     begin
         if reset_n = '0' then
             count    <= (others => '0');
-            tick_1hz <= '0';
+            tick_reg <= '0';
 
         elsif rising_edge(clk_in) then
 
-            if count = DIVISOR - 1 then
+            -- Only toggle at DIVISOR/2 for 1 Hz 50% duty cycle
+            if count = DIVISOR/2 - 1 then
+                tick_reg <= not tick_reg;
                 count    <= (others => '0');
-                tick_1hz <= '1';          -- pulse for one cycle
 
             else
-                count    <= count + 1;
-                tick_1hz <= '0';          -- low all other cycles
+                count <= count + 1;
             end if;
 
         end if;
     end process;
+
+    tick_1hz <= tick_reg;   -- continuous square wave
 
 end rtl;
